b3ffaa8eb886ab9062ada78851ac105c1d44d2d7
tux3 <barrdetwix@gmail.com>
2015-11-10 04:30:12 +0100

    Upload packages

--- sourceify.sh
+++ sourceify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Downloads the upstream tarball and sets up a source package
-NAME=libtoxcore
-UVER=0.1
+NAME=qtox
+UVER=1.1
 DVER=1
-wget https://github.com/irungentoo/toxcore/archive/master.tar.gz
+wget https://github.com/tux3/qTox/archive/master.tar.gz
 tar xf master.tar.gz
 rm master.tar.gz
-mv toxcore-master "$NAME"-"$UVER"
+mv qTox-master "$NAME"-"$UVER"
 tar cfz "$NAME"_"$UVER".orig.tar.gz "$NAME"-"$UVER"
 tar xvf "$NAME"_"$UVER"-"$DVER".debian.tar.xz -C "$NAME"-"$UVER"
 dpkg-source -b "$NAME"-"$UVER"
