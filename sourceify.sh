e98aba17357672427cd8fed2250dadcee21ae480
tux3 <barrdetwix@gmail.com>
2015-12-11 00:27:20 +0100

    Update versionning scheme

--- sourceify.sh
+++ sourceify.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # Downloads the upstream tarball and sets up a source package
 NAME=qtox
-UVER=1.1
+GIT_URL=https://github.com/tux3/qtox
+
+git clone $GIT_URL $NAME
+cd $NAME
+UVER=`git describe --tags`
 DVER=1
-wget https://github.com/tux3/qTox/archive/master.tar.gz
-tar xf master.tar.gz
-rm master.tar.gz
-mv qTox-master "$NAME"-"$UVER"
+cd ..
+
+mv $NAME "$NAME"-"$UVER"
+mv "$NAME".debian.tar.xz "$NAME"_"$UVER"-"$DVER".debian.tar.xz
 tar cfz "$NAME"_"$UVER".orig.tar.gz "$NAME"-"$UVER"
 tar xvf "$NAME"_"$UVER"-"$DVER".debian.tar.xz -C "$NAME"-"$UVER"
+sed s/UVER/$UVER/ -i "$NAME"-"$UVER"/debian/changelog
+sed s/DVER/$DVER/ -i "$NAME"-"$UVER"/debian/changelog
 dpkg-source -b "$NAME"-"$UVER"
-
