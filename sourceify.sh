85d4bd887f50b749ade6364944a233dbd3e69065
tux3 <barrdetwix@gmail.com>
2015-12-11 00:16:42 +0100

    Change versionning scheme

--- sourceify.sh
+++ sourceify.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # Downloads the upstream tarball and sets up a source package
-wget https://github.com/irungentoo/filter_audio/archive/master.tar.gz
-tar xf master.tar.gz
-rm master.tar.gz
-mv filter_audio-master libfilteraudio-0.1
-tar cfz libfilteraudio_0.1.orig.tar.gz libfilteraudio-0.1
-tar xvf libfilteraudio_0.1-1.debian.tar.xz -C libfilteraudio-0.1/
-dpkg-source -b libfilteraudio-0.1/
+NAME=libfilteraudio
+GIT_URL=https://github.com/irungentoo/filter_audio
 
+git clone $GIT_URL $NAME
+cd $NAME
+UVER=0.1.`git rev-list --count HEAD`.`git rev-parse HEAD | head -c 7`
+DVER=1
+cd ..
+
+mv $NAME "$NAME"-"$UVER"
+mv "$NAME".debian.tar.xz "$NAME"_"$UVER"-"$DVER".debian.tar.xz
+tar cfz "$NAME"_"$UVER".orig.tar.gz "$NAME"-"$UVER"
+tar xvf "$NAME"_"$UVER"-"$DVER".debian.tar.xz -C "$NAME"-"$UVER"
+sed s/UVER/$UVER/ -i "$NAME"-"$UVER"/debian/changelog
+sed s/DVER/$DVER/ -i "$NAME"-"$UVER"/debian/changelog
+dpkg-source -b "$NAME"-"$UVER"
